8937a93fc4c53f571fe1565779fcb6bc319ec953
TheNeonHaze <ahmetsalim_s@mail.ru>
2020-08-09 20:20:15 +0600

    main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from yandex_music.client import Client
+import eyed3
 import config
 import logging
 
@@ -14,14 +15,26 @@ class Downloader:
     def tracks_parse(self):
         likes_tracks = [f"{track.id}:{track.album_id}" for track in
                         self.client.users_likes_tracks().tracks]
-        tracks = [track.title for track in self.client.tracks(likes_tracks)]
+        tracks = []
+        for track in self.client.tracks(likes_tracks):
+            full_name = ''
+            try:
+                full_name += track.artists[0]['name'] + " - "
+            except IndexError:
+                full_name += "Какашка" + " - "
+            full_name += track.title
+            tracks.append(full_name)
+
+
         return tracks
 
     def download_track(self, track_id, album_id):
         track = self.client.tracks([f'{track_id}:{album_id}'])
         track = track[0]
-        track.download(track.title)
+        track.download(track.title+'.mp3', bitrate_in_kbps=320)
+
 
 
 bot = Downloader(config.login, config.password)
 print(bot.tracks_parse())
+#bot.download_track(60292250,10327453)
