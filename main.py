754fc2de0cebc1bcd9da6a5f7dda7b6df4152cf4
TheNeonHaze <ahmetsalim_s@mail.ru>
2020-08-09 18:52:18 +0600

    main

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
-import requests
+from yandex_music.client import Client
+import config
 
+#client = Client.from_credentials('example@yandex.com', 'password')
+
+class downloader:
+    def __init__(self, login, password):
+        self.client = Client.from_credentials(login, password)
+
+
+    def tracks_parse(self):
+        likes_tracks = [track.id for track in self.client.users_likes_tracks()]
+        my_tracks = self.client.tracks(likes_tracks)
+        tracks = []
+
+        for track in my_tracks:
+            title = track.title
+            artist = track.artists[0]['name']
+            fullname = title + " " + artist
+            tracks.append(fullname)
+
+        return tracks
+
+bot = downloader(config.name, config.password)
+bot.tracks_parse()
